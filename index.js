98251f74486ccfcc0b8d1159fc05445dcc54552f
satwikm <misra.sa@northeastern.edu>
2023-09-10 18:12:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { DISCORD_TOKEN: token } = process.env;
+
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    }
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`Warning: command at ${filePath} is missing a "data" or "execute" property`);
+    }
+}
+
+client.login(token);
